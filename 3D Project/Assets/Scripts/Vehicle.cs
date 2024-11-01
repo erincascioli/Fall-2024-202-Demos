7f19e24e0e006682249747a35369d61649621226
Erika Mesh <esmvcs@rit.edu>
2024-10-31 22:21:25 -0400

    Refactored the Vehicle script
    Calc accel as a % of maxSpeed & decel as a % change to the velocity
    Also some field refactoring to show ranges, tool tips, etc.

--- 3D Project/Assets/Scripts/Vehicle.cs
+++ 3D Project/Assets/Scripts/Vehicle.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// https://docs.google.com/document/d/1Igukg_XBW6FtwrM2sL0n3BkryQwu6pYfwAX64tpI40M/edit?tab=t.0#heading=h.w14grdblwqpa
+// https://docs.google.com/document/d/1n0c7FayT74mB6qEMTrCZQlP-Z4Nez4H2jtH6zxWCZ8U/edit?tab=t.0#heading=h.w14grdblwqpa
 public class Vehicle : MonoBehaviour
 {
     // Although vehicle will move with us "taking over", we'll still be 
     //   manipulating the Rigidbody of the vehicle while it's kinematic.
-    public Rigidbody rBody;
+    [SerializeField] Rigidbody rBody;
 
-    // Fields for Speed (per second)
-    public float maxSpeed;
-    public float minSpeed;
+    // Fields for Speed
+    [SerializeField] float maxSpeed;
+    [SerializeField, Tooltip("Speed below which the vehicle will come to a full stop.")] float minSpeed;
 
-    // Fields for Acceleration & Deceleration
-    public float accelerationRate;
-    public float decelerationRate;
+    // Fields for Acceleration/Deceleration
+    [SerializeField, Range(0, 1), Tooltip("The % of the max speed the vehicle can achieve in 1 second.")] float accelPercent;
+    [SerializeField, Range(0, 1), Tooltip("The % by which the velocity is reduced during 1 second of 0 acceleration.")] float decelPercent;
+
+    // Fields for Acceleration & Deceleration can also be in units per second
+    // This is how we originally did it in class.
+    /*
+    [SerializeField] float accelerationRate;
+    [SerializeField] float decelerationRate;
+    */
 
     // Fields for Turning
-    public float turnSpeed;
+    [SerializeField, Tooltip("The # of degrees/second a moving vehicle can turn.")] float turnSpeed;
 
     // ***
     // All remaining fields are private!
@@ -27,15 +36,14 @@ public class Vehicle : MonoBehaviour
     // ***
 
     // Fields for Input
-    public Vector3 movementDirection;
+    private Vector3 movementDirection;
 
     // Fields for Movement Vectors
-    [SerializeField]
     private Vector3 velocity;
     private Vector3 acceleration;
 
     // Fields for Quaternions
-    private Quaternion turning;
+    private Quaternion turnAmount;
 
 
     // Start is called before the first frame update
@@ -58,8 +66,11 @@ public class Vehicle : MonoBehaviour
             // Movement "formula":
             //transform.forward = movementDirection;
 
-            // accel 
-            acceleration = accelerationRate * movementDirection.z * transform.forward;
+            // accel based on units per second
+            //acceleration = accelerationRate * movementDirection.z * transform.forward;
+
+            // accel based on % of max speed
+            acceleration = transform.forward * movementDirection.z * accelPercent * maxSpeed;
 
             // Velocity is speed * direction - not scaled to a per-frame basis.
             // It's on a per-second basis.
@@ -72,9 +83,14 @@ public class Vehicle : MonoBehaviour
         }
         else // DECEL
         {
-            velocity *= 1f - (decelerationRate * Time.fixedDeltaTime);
+            // decel based on units per second
+            // velocity *= 1f - (decelerationRate * Time.fixedDeltaTime);
 
-            if(velocity.sqrMagnitude < minSpeed * minSpeed)
+            // decelerate based on % of velocity kept per second
+            velocity *= 1 - (decelPercent * Time.fixedDeltaTime);
+
+            // If it gets really small, just zero it out
+            if (velocity.sqrMagnitude < minSpeed * minSpeed)
             {
                 velocity = Vector3.zero;
             }
